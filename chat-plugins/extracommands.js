30caeba8b67360f59d8b7b0e15be200aa36dd04f
Ricardo253 <enzocarta28@gmail.com>
2018-10-09 12:38:05 +0200

    new commands

--- chat-plugins/extracommands.js
+++ chat-plugins/extracommands.js
@@ -1,5 +1,18 @@
 'use strict';
 
+exports.commands = {
+	cc: 'customcolour',
+	customcolour: function (target, room) {
+		let targets = target.split(',');
+		if (targets.length < 2) return this.sendReply("/customcolour OR /cc [colour], [message] - Outputs a message in a custom colour. Requires: " + Users.getGroupsThatCan('customcolour', room).join(" "));
+		if (!this.can('customcolour', room) || !this.canBroadcast('!cc')) return false;
+
+		this.sendReply('|raw|<font color="' + targets[0].toLowerCase().replace(/[^#a-z0-9]+/g, '') + '">' + Tools.escapeHTML(targets.slice(1).join(",")) + '</font>');
+	},
+};
+
+'use strict';
+
 let fs = require('fs');
 
 function reloadCustomAvatars() {
