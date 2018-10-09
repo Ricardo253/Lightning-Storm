1b3e8bbada9ebbb0abded8cc7894dc0cea1d838a
Ricardo253 <enzocarta28@gmail.com>
2018-10-09 17:13:17 +0200

    new commands

--- chat-plugins/extracommands.js
+++ chat-plugins/extracommands.js
@@ -178,3 +178,22 @@ exports.commands = {
 		connection.popup(buffer.join("\n\n"));
 	},
 };
+
+clearall: function (target, room, user) {
+		if (!this.can('declare')) return false;
+		if (room.battle) return this.sendReply("You cannot clearall in battle rooms.");
+
+		clearRoom(room);
+	},
+};
+	
+   gclearall: 'globalclearall',
+	globalclearall: function (target, room, user) {
+		if (!this.can('gdeclare')) return false;
+
+		for (let u in Users.users) {
+			Users.users[u].popup("All rooms are being clear.");
+		}
+		Rooms.rooms.forEach(clearRoom);
+	},
+};	
