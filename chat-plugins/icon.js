81fac134244cd3582c07530357a43b2dad0dbcfe
Ricardo253 <enzocarta28@gmail.com>
2018-10-09 13:25:03 +0200

    Update icon.js

--- chat-plugins/icon.js
+++ chat-plugins/icon.js
@@ -79,7 +79,7 @@ exports.commands = {
 		if (icons[toId(target[0])]) return this.errorReply("This user already has a custom userlist icon.  Do /icon [user], delete and then set their new icon.");
 		this.sendReply("|raw|You have given <b><font color=" + color(Chat.escapeHTML(target[0])) + ">" + Chat.escapeHTML(target[0]) + "</font></b> an icon.");
 		Rooms('staff').add('|raw|<b><font color="' + color(Chat.escapeHTML(target[0])) + '">' + Chat.escapeHTML(target[0]) + '</font> has received an icon from ' + Chat.escapeHTML(user.name) + '.</b>').update();
-		this.privateModCommand("(" + target[0] + " has recieved icon: '" + target[1] + "' from " + user.name + ".)");
+		this.privateModAction("(" + target[0] + " has recieved icon: '" + target[1] + "' from " + user.name + ".)");
 		icons[toId(target[0])] = target[1];
 		updateIcons();
 	},
